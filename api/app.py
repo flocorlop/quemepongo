b2a2986dbfc2b991e26dcdeac9be7fd26946baaa
Flor <flocorlop@alum.us.es>
2022-03-27 22:09:28 +0200

    Añadido modelo
    model keras

--- api/app.py
+++ api/app.py
@@ -3,6 +3,20 @@ import json
 from profiles import profiles
 from outfits import outfits
 from Models import db, Outfits
+
+#region imports predictions:
+import base64
+import numpy as np
+import io
+from PIL import Image
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
+#endregion
+
 app = Flask(__name__)
 
 #config bbdd
@@ -93,5 +107,45 @@ def getOutfit(id):
 
 
 #endregion
+
+
+#region Model y predictions
+def get_model():
+    global model
+    model = load_model('modelft_outfits.h5')
+    print(" Model loaded")
+
+def preprocess_image(image, target_size):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+print ("Loading keras model")
+get_model()
+
+@app.route("/outfits/new-outfit/predict", methods=["POST"])
+def predict():
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(enconded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size(224,224))
+
+    prediction = model.predict(processed_image).tolist()
+
+    response = {
+        'prediction_percentage': prediction[0][0]
+        }
+    
+    return jsonify(response)
+
+#endregion
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
+
